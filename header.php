911c3622beec552e15e23693f8d481fb4b8f694d
kanjo <kanjo.school@gmail.com>
2020-10-24 18:29:03 -0400

    Implémentation du login system

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ô'GÎTES</title>
+    </head>
+    <head>
+        <h1>Bonjour</h1>
+    </head>
+    <body>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php  require_once 'header.php'; ?>
+<input type="text" placeholder="Rechercher un lieu">
+<button>Rechercher</button>
+<?php require_once 'footer.php'; ?> 
