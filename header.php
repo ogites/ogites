11bbed365965341151b8685eb1c2ddfabdf11b6f
kanjo <kanjo.school@gmail.com>
2020-10-26 00:45:35 -0400

    petit soucis de style à régler

--- header.php
+++ header.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ô'GÎTES</title>
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="style.css" type="text/css"/>
     </head>
     <header>
         <h1>OGITES</h1>
-        <nav>
+        <nav id="top-menu">
             <ul>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">Prensentation</a></li>
+                <li><a href="#">Presentation</a></li>
                 <li><a href="#">About us</a></li>
+                <li><a href="#">Register</a></li>
             </ul>
         </nav>
     </header>
--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php  require_once 'header.php'; ?>
 
-<body class="main-container">
-    <input type="text" placeholder="Rechercher un lieu">
-    <button>Rechercher</button>
-    <p>YO bonjour</p>
+<body>
+    <input class="c-checkbox" type="checkbox" id="checkbox">
+    <div class="c-formContainer">
+        <form class="c-form" action="">
+            <input class="c-form__input" placeholder="Trouver un gîte" type="text" required>
+            <label class="c-form__buttonLabel" for="checkbox">
+                <button class="c-form__button" type="button">Rechercher</button>
+            </label>
+            <label class="c-form__toggle" for="checkbox" data-title="Recherche"></label>
+        </form>
+    </div>
 </body>
 
 <?php require_once 'footer.php'; ?> 
