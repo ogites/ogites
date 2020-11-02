959ae34f0b2cae2798a52b74f4c53f0885d8ece1
kanjo <kanjo.school@gmail.com>
2020-11-02 11:28:36 -0400

    Ajout d'images dans slideshow

--- assets/slideshow.php
+++ assets/slideshow.php
@@ -3,5 +3,6 @@
     <img src="./images/gwada/img2.jpg">
     <img src="./images/gwada/img3.jpg">
     <img src="./images/gwada/img4.jpg">
+    <img src="./images/gwada/img5.jpg">
     <img src="./images/gwada/img1.jpg">
 </div>
